d3680e0b2321947b32ea116f9cc5f39439894eec
en20 <424021424@qq.com>
2018-08-23 18:05:06 +0800

    添加自己的tilemap

--- src/scene/tileMapScene.js
+++ src/scene/tileMapScene.js
@@ -11,10 +11,7 @@ export default class tileMapScene extends Phaser.Scene {
     preload() {
         this.load.image('sky', 'resource/image/sky.png')
 
-        this.load.tilemapTiledJSON('map', 'resource/tilemap/level-with-coin-objects.json');
-        this.load.spritesheet('coin', 'resource/image/coin.png', {frameWidth: 32, frameHeight: 32});
-        this.load.image('ground_1x1', 'resource/image/ground_1x1.png');
-
+        // link sprite
         this.load.spritesheet("link", 'resource/image/link.png', {frameWidth: 32, frameHeight: 32})
 
         //  Mario 地图
@@ -24,33 +21,79 @@ export default class tileMapScene extends Phaser.Scene {
         this.load.tilemapTiledJSON('map3', 'resource/tilemap/super-mario-3.json');
         this.load.image('tiles3', 'resource/image/super-mario-3.png');
 
+        // 带 object 的 mario 地图
+        this.load.tilemapTiledJSON({key: 'level1', url: 'resource/tilemap/level1.json'});
+        this.load.spritesheet('brick', 'resource/img/Levels/brick.png', {frameWidth: 16, frameHeight: 16});
+        this.load.spritesheet('brick2', 'resource/img/Levels/brick2.png', {frameWidth: 16, frameHeight: 16});
+        this.load.spritesheet('brickCoin', 'resource/img/Levels/questionMarkBlock.png', {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+        this.load.spritesheet('brickCoin2', 'resource/img/Levels/questionMarkBlock_2.png', {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+        this.load.spritesheet('brickCoins', 'resource/img/Levels/brickCoins.png', {frameWidth: 16, frameHeight: 16});
+        this.load.spritesheet('brickCoins2', 'resource/img/Levels/brickCoins_2.png', {frameWidth: 16, frameHeight: 16});
+        this.load.image('tileset_levels', 'resource/tilemap/tileset_levels.png');
+
+        //  自己制作的tilemap
+        this.load.tilemapTiledJSON({key: 'test_tilemap', url: 'resource/tilemap/test_tilemap.json'});
+        this.load.tilemapTiledJSON({key: 'test_tilemap2', url: 'resource/tilemap/test_tilemap_2.json'});
+        this.load.image("sheet_tileset", "resource/image/sheet_tileset.png")
+        this.load.image("super-mario_tileset", "resource/image/super-mario.png")
+        // this.load.tilemapCSV('map', 'resource/tilemap/test_tilemap_groundLayer.csv');
+
+
     }
 
     create() {
-        this.add.image(0, 0, 'sky').setOrigin(0, 0)
+        // this.add.image(0, 0, 'sky').setOrigin(0, 0)
 
-        // 智障小人地图
-        // let map = this.add.tilemap('map');
-        // let tiles = map.addTilesetImage('ground_1x1');
-        // this.layer = map.createStaticLayer('Tile Layer', tiles);
-        // this.anims.create({
-        //     key: 'spin',
-        //     frames: this.anims.generateFrameNumbers('coin', {start: 0, end: 6}),
-        //     frameRate: 16,
-        //     repeat: -1
-        // });
-        // this.coins = map.createFromObjects('Coin Object Layer', 26, {key: 'coin'});
-        // this.anims.play('spin',  this.coins);
-        //  智障小人地图 END
+        // 带 object 的 mario 地图
+        // this.map = this.add.tilemap('level1');
+        // let tileset_level1 = this.map.addTilesetImage('tileset_levels');
+        // this.graphicLayer = this.map.createDynamicLayer('Graphic_Layer',tileset_level1,0,0);
 
+        // this.backgroundColor = this.map.createDynamicLayer('Background_Color',tileset_level1,0,0);
+        // this.pipesAccessLevelLayer =this.map.createDynamicLayer('PipesAccessLevel',tileset_level1,0,0);
+        // this.pipesAccessLayer = this.map.createDynamicLayer('PipesAccess',tileset_level1,0,0);
+        // this.exitPipesLayer = this.map.createDynamicLayer('ExitPipes',tileset_level1,0,0);
+        // this.finishLevelLayer =this.map.createDynamicLayer('FinishLevel',tileset_level1,0,0);
+
+        // let tileset_brickCoin =  this.map.addTilesetImage('brickCoin');
+        // this.BricksCoinLayer =this.map.createDynamicLayer('brickCoin',tileset_brickCoin,0,0);
+        // this.brickCoinsGroup = this.physics.add.group()
+
+        //  自己制作的tilemap
+        // this.map = this.add.tilemap('test_tilemap');
+        // let sheet_tileset = this.map.addTilesetImage('sheet_tileset');
+        // this.groundLayer = this.map.createDynamicLayer('groundLayer', sheet_tileset, 20, 20);
+        // this.gunLayer = this.map.createStaticLayer('gunLayer', sheet_tileset, 0, 0);
+        // this.groundLayer.resizeWorld();
+        // log(this.map)
+
+        // When loading a CSV map, make sure to specify the tileWidth and tileHeight
+        let map = this.add.tilemap('test_tilemap2');
+        // let map = this.make.tilemap({key: 'map', tileWidth: 16, tileHeight: 16});
+        var sheet_tileset = map.addTilesetImage('super-mario_tileset');
+        this.groundLayer = map.createStaticLayer(0, sheet_tileset, 0, 0);
+
+        // this.groundLayer.setCollision([14,16,])
+        this.groundLayer.setCollisionByProperty({collides: true})
+
+        log(map)
+
+        // 旧 Mario 地图
         var map1 = this.make.tilemap({key: 'map1'});
         var tileset1 = map1.addTilesetImage('SuperMarioBros-World1-1', 'tiles1');
         // this.layer1 = map1.createStaticLayer('World1', tileset1, 0, 0);
-        this.layer1 = map1.createDynamicLayer('World1', tileset1, 0, 0);
+        this.layer1 = map1.createDynamicLayer('World1', tileset1, 0, 600);
 
-        // var map2 = this.add.tilemap('map3');
-        // var tileset2 = map2.addTilesetImage('SuperMarioBrosMap1-3_bank.png', 'tiles3');
-        // var layer2 = map2.createStaticLayer('ShoeBox Tile Grab', tileset2, 700, 300);
+        let map2 = this.add.tilemap('map3');
+        var tileset2 = map2.addTilesetImage('SuperMarioBrosMap1-3_bank.png', 'tiles3');
+        var layer2 = map2.createStaticLayer('ShoeBox Tile Grab', tileset2, 0, 300);
+
 
         this.layer1.setCollisionByExclusion([1, 2, 3, 12, 11])
 
@@ -74,13 +117,30 @@ export default class tileMapScene extends Phaser.Scene {
 
         // 调试内容
         this.showDebug = true
-        this.debugGraphics = this.add.graphics();
+        this.debugGraphics = this.add.graphics()
+        // new Phaser.GameObjects.Graphics(this,{x:0, y:600}); //   无法使用???
+        this.debugGraphics.y = 600
+
         this.drawDebug()
         this.input.keyboard.on('keydown_U', (event) => {
             this.showDebug = !this.showDebug;
             this.drawDebug();
         });
 
+
+        // new player
+        this.player = new PlayerSprite({
+            scene: this,
+            x: 0,
+            y: 0,
+        });
+        this.physics.add.collider(this.player, this.layer1)  // player 与 layer 碰撞
+        this.physics.add.collider(this.player, this.graphicLayer)
+        this.physics.add.collider(this.player, this.groundLayer)    // 自己建的地图ground
+        this.player.setCollideWorldBounds(true) // 世界碰撞
+
+        // 镜头跟随,开启后镜头控制会被覆盖
+        // this.cameras.main.startFollow(this.player);
         //  camera 镜头控制
         let cursors = this.input.keyboard.createCursorKeys();
         let SmoothedKeyControlConfig = {
@@ -105,25 +165,23 @@ export default class tileMapScene extends Phaser.Scene {
         }
         this.controls = new Phaser.Cameras.Controls.FixedKeyControl(FixedKeyControlConfig);
         this.cameras.main.setBackgroundColor(0x6888ff)
-        // new player
-        this.player = new PlayerSprite({
-            scene: this,
-            x: 330,
-            y: 80,
-        });
-        this.physics.add.collider(this.player, this.layer1)  // player 与 layer 碰撞
-        this.player.setCollideWorldBounds(true) // 世界碰撞
+        // this.cameras.main.setBounds(0, 0, this.layer1.x + this.layer1.width + 600, 0);
 
-        this.cameras.main.startFollow(this.player);  // 镜头跟随
-        // this.cameras.main.setBounds(0, 0, layer1.x + layer1.width + 600, 0);
-
-        this.scoreText = this.add.text(0, 0, "score : 0")
+        /**
+         * 滚动因子控制相机移动对此游戏对象的影响。
+         当相机滚动时，它将改变在屏幕上呈现此游戏对象的位置。
+         它不会更改游戏对象的实际位置值。
+         *  值为1表示它将与摄像机完全同步移动。
+         值为0表示即使相机移动也不会移动。
+         但实际测试为0会随着镜头移动而移动,不论设置为0还是1 or 其他 文本的x,y都不改变
+         */
+        this.scoreText = this.add.text(0, 0, "score : 0").setScrollFactor(0);
 
 
         // 事件的监听
         this.events.on('drawDebugEvent', function () {
             log("this.events.on('drawDebugEvent'")
-        }, this); //watch for Game Over
+        }, this)
 
     }
 
@@ -139,7 +197,7 @@ export default class tileMapScene extends Phaser.Scene {
     drawDebug() {
         this.debugGraphics.clear()
         if (this.showDebug) {
-            this.layer1.renderDebug(this.debugGraphics, {
+            this.groundLayer.renderDebug(this.debugGraphics, {
                 tileColor: null, // Non-colliding tiles
                 collidingTileColor: new Phaser.Display.Color(0, 0, 0, 50), // Colliding tiles
                 // faceColor:  new Phaser.Display.Color(0, 0, 255, 50), // Colliding face edges
